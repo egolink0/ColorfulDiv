cf4991520f13b010b2e47abf69a6a0d254ae670a
egolink0 <18521972+egolink0@users.noreply.github.com>
2023-12-05 15:33:37 +0800

    muti file output types

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,21 @@ export default defineConfig({
     lib: {
         entry: path.resolve(__dirname, 'src/packages/main.ts'),
         name: 'colorful-div',
+        formats: ['es', 'umd', 'cjs'],
         fileName: (format) => `lib.${format}.js`
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['react', 'react-dom'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
     },
   }
 })
